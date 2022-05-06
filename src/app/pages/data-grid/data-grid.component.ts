68ee9805ad51f13f3b2f25536cbbc1a58a2561aa
max <teerawat261239@gmail.com>
2022-05-06 18:43:55 +0700

    add table

--- src/app/pages/data-grid/data-grid.component.ts
+++ src/app/pages/data-grid/data-grid.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { NgModule, Component, enableProdMode } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { DxDataGridModule } from 'devextreme-angular';
+import { Order, Service } from 'src/app/shared/services/app-service';
 
 @Component({
   selector: 'app-data-grid',
   templateUrl: './data-grid.component.html',
-  styleUrls: ['./data-grid.component.scss']
+  styleUrls: ['./data-grid.component.scss'],
+  providers: [Service],
 })
-export class DataGridComponent implements OnInit {
+export class DataGridComponent {
+  orders: Order[];
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(service: Service) {
+    this.orders = service.getOrders();
   }
-
 }
